cb22d54244972eeee0d8c16466f987fe3c237c58
Bryan <Bryan@DESKTOP-VJ7HQ51>
2020-10-04 16:39:00 -0700

    Added functionality to methods and tested output

--- cse360assignment02/src/cse360assignment02/AddingMachine.java
+++ cse360assignment02/src/cse360assignment02/AddingMachine.java
@@ -2,36 +2,50 @@ package cse360assignment02;
 
 public class AddingMachine {
   private int total;
+  private String s;
   
   // constructor 
   public AddingMachine () {
     total = 0;  // not needed - included for clarity
+    s = "0";
   }
   
   // getter to retrieve total 
   public int getTotal () {
-    return 0;
+	  
+    return total;
   }
   
   // mutator adds to total 
   public void add (int value) {
+	  total = total + value;
+	  s = s + " + " + value;
   }
 
   // mutator subtracts from total 
   public void subtract (int value) {
+	  total = total - value;
+	  s = s + " - " + value;
   }
   // getter prints out data in a string format
   public String toString () {
-    return "";
+    return s;
   }
   
   // mutator clears data 
-  public void clear() {
+public void clear() {
+	  total = 0;
+	  s = "0";
   }
   
   public static void main(String[] args) {
 		// TODO Auto-generated method stub
-	  	System.out.println("test case 1 ");
+	  AddingMachine calculator = new AddingMachine();
+	  calculator.add(4);
+	  calculator.subtract(2);
+	  calculator.add(5);
+	  System.out.println(calculator.toString());
+	  System.out.println(calculator.getTotal());
 	}
 
 }
