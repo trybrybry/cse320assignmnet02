e316910c1b2c8530c9aa490c22bc6bdf76af0e57
Bryan Alfaro <brianalf3@gmail.com>
2020-10-04 16:09:14 -0700

    Second change added main and commented code

--- cse360assignment02/src/cse360assignment02/AddingMachine.java
+++ cse360assignment02/src/cse360assignment02/AddingMachine.java
@@ -3,24 +3,35 @@ package cse360assignment02;
 public class AddingMachine {
   private int total;
   
+  // constructor 
   public AddingMachine () {
     total = 0;  // not needed - included for clarity
   }
   
+  // getter to retrieve total 
   public int getTotal () {
     return 0;
   }
   
+  // mutator adds to total 
   public void add (int value) {
   }
 
+  // mutator subtracts from total 
   public void subtract (int value) {
   }
-
+  // getter prints out data in a string format
   public String toString () {
     return "";
   }
-
+  
+  // mutator clears data 
   public void clear() {
   }
+  
+  public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	  	System.out.println("test 1");
+	}
+
 }
